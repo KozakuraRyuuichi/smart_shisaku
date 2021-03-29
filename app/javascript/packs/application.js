b3b4b39a06b01bb286586ce7b3139e9c169258fb
KozakuraRyuuichi <k267280.352@gmail.com>
2021-03-29 23:53:14 +0900

    [add]WebpackにjQueryとBootstrapを導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// import Rails from "@rails/ujs"
+// import * as ActiveStorage from "@rails/activestorage"
+// import "channels"
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
 
-Rails.start()
-ActiveStorage.start()
+import 'bootstrap';
+// import 'bootstrap-material-design'
+import '../stylesheets/application.scss'
+
+
+// Rails.start()
+// ActiveStorage.start()
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
